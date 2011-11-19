42019d8827dab5ccc3075f7a2ac300016a8a0bd4
Orson Peters <nightcracker@nclabs.org>
2011-11-19 23:02:21 +0100

    pyglet version - some work

--- map.py
+++ map.py
@@ -1,22 +1,22 @@
-#!/usr/bin/env python
-
 from __future__ import division, print_function
 
+import pyglet
+from pyglet.gl import *
 import function
-import pygame
 
 class Map(object):
     def __init__(self, game, mapname):
         self.mapname = mapname
     
-        self.image = function.load_image("maps/" + mapname)
-        self.image = pygame.transform.scale(self.image, (self.image.get_width()*6, self.image.get_height()*6))
-        self.image.set_colorkey(None)
-        self.image = self.image.convert()
+        self.image = pyglet.sprite.Sprite(function.load_image("maps/" + mapname))
+        self.image.scale = 6
         
         self.collision_mask = function.load_mask("collisionmaps/" + mapname)
         x, y = self.collision_mask.get_size()
         self.collision_mask = self.collision_mask.scale(x*6, y*6)
-
-    def draw(self, game):        
-        game.window.blit(self.image, (0, 0), (game.xview, game.yview, game.view_width, game.view_height))
+        
+    def draw(self, game):
+        self.image.x = -game.xview
+        self.image.y = -game.yview
+        
+        self.image.draw()
