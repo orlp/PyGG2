c5cecfb35655054de11d8e635b65c3a752abfe02
Orson Peters <nightcracker@nclabs.org>
2011-11-20 00:24:28 +0100

    changed animation of legs

--- character.py
+++ character.py
@@ -111,8 +111,15 @@ class ScoutDrawer(entity.EntityDrawer):
             self.animoffset += frametime * abs(character.hspeed) / 20
             self.animoffset %= 2
             anim_frame = int(self.animoffset)
+            
+            if anim_frame == 1 and not character.onground(game, state):
+                anim_frame = 1
+                self.animoffset = 1.0
         
-        image = self.sprites[anim_frame+(2 if character.intel else 0)]
+        if character.intel:
+            anim_frame += 2
+        
+        image = self.sprites[anim_frame]
         
         if character.flip: image = pygame.transform.flip(image, 1, 0)
         
