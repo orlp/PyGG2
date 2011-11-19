b37e1f17bdd30216305e7c378eee194d6ebcd43e
Orson Peters <nightcracker@nclabs.org>
2011-11-20 00:39:42 +0100

    added constant for sending fps

--- constants.py
+++ constants.py
@@ -8,3 +8,4 @@ import function
 # this file contains all kinds of constants
 
 PHYSICS_TIMESTEP = 1/50 # always update physics in these steps
+INPUT_SEND_FPS = 1/30 # we send input to the server at this rate
