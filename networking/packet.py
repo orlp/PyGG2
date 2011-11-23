70cca26e65f8495f190edcd631ed8368a8e9830e
Orson Peters <nightcracker@nclabs.org>
2011-11-23 17:37:51 +0100

    created packet system

---
+++ networking/packet.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+from __future__ import division, print_function
+
+# add our main folder as include dir
+import sys
+sys.path.append("../")
+
+import struct
+import event
+
+class Packet(object):
+    def __init__(self, sender):
+        self.sequence = None
+        self.acksequence = None
+        self.events = []
+        self.sender = sender
+
+    def pack(self):
+        packetstr = ""
+
+        packetstr += struct.pack("!HH", sequence, acksequence)
+
+        for event in self.events:
+            packetstr += event.pack()
+
+        return packetstr
+
+    def unpack(self, packetstr):
+        self.events = []
+
+        self.sequence, self.acksequence = struct.unpack_from("!HH", packetstr)
+        packetstr = packetstr[struct.calcsize("!HH"):]
+
+        while packetstr:
+            eventid = struct.unpack_from("!B", packetstr)
+            packetstr = packetstr[struct.calcsize("!B"):]
+
+            if self.sender == "client":
+                event = object.__new__(event.clientevents[eventid])
+            else:
+                event = object.__new__(event.serverevents[eventid])
+
+            eventsize = event.unpack(packetstr)
+            packetstr = packetstr[eventsize:]
+
+            self.events.append(event)
