437108619afa5d8e1465d6abc2dd8c6d7d0c2204
Orpheon <alex.liebendorf@gmail.com>
2012-01-28 14:50:48 +0100

    Added ingame pyro selection with "2".

--- engine/projectile.py
+++ engine/projectile.py
@@ -12,13 +12,13 @@ import mask
 
 class Shot(entity.MovingObject):
     shot_hitmasks = {}
-    
+
     fade_time = 0.8 # seconds of fading when max_flight_time is being reached
     max_flight_time = 1.5
 
     def __init__(self, game, state, sourceweapon, damage, direction, speed):
         super(Shot, self).__init__(game, state)
-        
+
         self.direction = 0.0
         self.flight_time = 0.0
         self.sourceweapon = sourceweapon
@@ -76,10 +76,10 @@ class Shot(entity.MovingObject):
 
 class Rocket(entity.MovingObject):
     rocket_hitmasks = {}
-    
+
     fade_time = .3 # seconds of fading when max_flight_time is being reached
     max_flight_time = 20
-    
+
     damage = 35
     blastradius = 65
     knockback = 70000
@@ -147,55 +147,55 @@ class Rocket(entity.MovingObject):
 
 class Flame(entity.MovingObject):
     flame_hitmasks = {}
-    max_flight_time = 0.3 #I have no idea what the actual value is :P
+    max_flight_time = 0.5 - random.randint(0, 3)/10
     damage = 3.3
-	
+
     def __init__(self, game, state, sourceweapon):
         super(Flame, self).__init__(game, state)
-        
+
         self.direction = 0.0
         self.flight_time = 0.0
         self.sourceweapon = sourceweapon
-        
+
         srcwep = state.entities[sourceweapon]
         srcchar = state.entities[srcwep.owner]
-		
+
         self.x = srcchar.x
         self.y = srcchar.y
-        
+
         self.direction = srcwep.direction
-        
+
         self.speed = (500/13)*(5+random.randint(0,5))
         self.hspeed = math.cos(math.radians(self.direction)) * self.speed
         self.vspeed = math.cos(math.radians(self.direction)) * -self.speed
-        
+
     def step(self, game, state, frametime):
         self.speed += 30 # Copied from GMK-GG2; should simulate some very basic acceleration+air resistance.
         #Gravitational force
         self.vspeed -= 4.5
-        
+
         #calculate direction
         self.direction = function.point_direction(self.x - self.hspeed, self.y - self.vspeed, self.x, self.y)
-    
+
     def endstep(self, game, state, frametime):
         super(Flame, self).endstep(game, state, frametime)
-        
+
         self.flight_time += frametime
-        
-        angle = int(self.direction) % 360
-        
+
+        angle = int(self.direction)
+
         if angle in self.flame_hitmasks:
             mask = self.flame_hitmasks[angle]
         else:
             mask = function.load_mask("projectiles/flames/0").rotate(angle)
             self.flame_hitmasks[angle] = mask
-            
+
         if game.map.collision_mask.overlap(mask, (int(self.x), int(self.y))) or self.flight_time > self.max_flight_time:
             self.destroy(state)
-            
-            
+
+
     def interpolate(self, prev_obj, next_obj, alpha):
         super(Flame, self).interpolate(prev_obj, next_obj, alpha)
         self.direction = function.interpolate_angle(prev_obj.direction, next_obj.direction, alpha)
-        
+
         self.flight_time = prev_obj.flight_time + (next_obj.flight_time - prev_obj.flight_time) * alpha
